addf039e735bfef473678fe8137a563eaf31c672
Castellanii <148513260+Castellanii@users.noreply.github.com>
2024-04-18 23:42:18 -0400

    ShapeShift's added

--- Assets/Scripts/KevinScripts/ShapeShiftInteractor.cs
+++ Assets/Scripts/KevinScripts/ShapeShiftInteractor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ShapeShiftInteractor : MonoBehaviour
 {
     [SerializeField] private GameObject gary;
-    //[SerializeField] private GameObject patrick;
+    [SerializeField] private GameObject patrick;
     [SerializeField] private GameObject player;
     [SerializeField] private float transformDuration = 10.0f;
 
@@ -18,23 +18,25 @@ public class ShapeShiftInteractor : MonoBehaviour
             //Destroy(other.gameObject);
             gary.SetActive(true);
             player.SetActive(false);
+            patrick.SetActive(false);
             Invoke("RevertTransformation", transformDuration);
         }
 
-        //if (other.gameObject.tag.Equals("Patrick"))
-        //{
-        //    Debug.Log($"Collided");
-        //    Destroy(other.gameObject);
-        //    patrick.SetActive(true);
-        //    player.SetActive(false);
-        //    Invoke("RevertTransformation", transformDuration);
+        if (other.gameObject.tag.Equals("Patrick"))
+        {
+            Debug.Log($"Collided");
+            //Destroy(other.gameObject);
+            patrick.SetActive(true);
+            player.SetActive(false);
+            gary.SetActive(false);
+            Invoke("RevertTransformation", transformDuration);
 
-        //}
+        }
     }
     void RevertTransformation()
     {
-        //patrick.SetActive(false);
         player.SetActive(true);
+        patrick.SetActive(false);
         gary.SetActive(false);
         
     }
