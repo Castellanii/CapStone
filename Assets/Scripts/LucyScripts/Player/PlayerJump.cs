5e1aff6afed2496f8162f2b058b7ad2578c25a0f
Lucycycy1207 <lucy.chen991207@gmail.com>
2024-04-20 04:43:10 -0700

    Add SpongeBob char, running and jump animation. Adjust switch lane so character is in smooth transfer.

--- Assets/Scripts/LucyScripts/Player/PlayerJump.cs
+++ Assets/Scripts/LucyScripts/Player/PlayerJump.cs
@@ -8,25 +8,50 @@ public class PlayerJump : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Rigidbody rb;
 
+    private Animator animator;
     public bool isGrounded { get; private set; }
 
+    private void Awake()
+    {
+        animator = this.GetComponent<Animator>();
+    }
     public void Jump()
     {
         if (!isGrounded) return;
-        //Debug.Log("jump");
-        this.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        Debug.Log("jump");
+        animator.SetTrigger("Jump");
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundMask);
+        if (isGrounded)
+        {
+
+            animator.SetBool("IsGrounded", true);
+        }
+        else
+        {
+            Debug.Log("false");
+            animator.SetBool("IsGrounded", false);
+        }
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
+        if (isGrounded)
+        {
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            Gizmos.color = Color.black;
+        }
+        
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckDistance);
     }
 }
