0fc2947fa0f0aa06d544da449d14875e39c65a83
Castellanii <148513260+Castellanii@users.noreply.github.com>
2024-04-21 12:19:01 -0400

    Added Salt and Hamburger.

---
+++ Assets/Scripts/KevinScripts/Hamburger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hamburger : MonoBehaviour, IPickable
+{
+    [SerializeField] private Material breakMaterial;
+    [SerializeField] private Material origMaterial;
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private float materialDuration;
+    private Renderer renderer;
+
+    private bool canBreak = false;
+
+    void Start()
+    {
+        renderer = player.GetComponent<Renderer>();
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            renderer.material = breakMaterial;
+            canBreak = true;
+            Invoke("RevertMaterial", materialDuration);
+        }
+    }
+
+    private void RevertMaterial()
+    {
+        renderer.material = origMaterial;
+        canBreak = false;
+    }
+
+
+}
