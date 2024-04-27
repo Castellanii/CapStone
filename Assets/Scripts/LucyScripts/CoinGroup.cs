1d895581bd0cf1575d89e8e6acd6297e407fcd68
Lucycycy1207 <lucy.chen991207@gmail.com>
2024-04-27 13:52:57 -0700

    Add Coin prefab group with obstacles. Randomly spawn in lanes and randomly select coin group and obstacles.

---
+++ Assets/Scripts/LucyScripts/CoinGroup.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinGroup : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> Obstacles;
+
+
+    public void GenerateObstacles(float possibility)
+    {
+        bool activate;
+        for (int i = 0; i < Obstacles.Count; i++)
+        {
+            activate = Random.Range(0f, 1f) <= possibility ? true : false;
+            if (activate)
+            {
+                Obstacles[i].SetActive(true);
+            }
+            
+        }
+    }
+
+}
